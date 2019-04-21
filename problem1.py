9f8351872606b1689a767c737902047d3b2f1e14
imzad <badredine.sayah@gmail.com>
2019-04-21 12:59:00 -0400

    Mwd_mlp correct activation, some clean and reorganizing

--- problem1.py
+++ problem1.py
@@ -1,20 +1,49 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 23 13:20:15 2019
-
-@author: chin-weihuang
-"""
 from __future__ import print_function
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 from torch.autograd import grad
-import matplotlib.pyplot as plt
-#from scipy.spatial import distance
 
-import samplers
+from samplers import *
+
+import matplotlib.pyplot as plt
+
+
+
+def jsd_objective(Discrim, x_p, y_q):
+
+    jsd_objective = torch.log(torch.Tensor([2])) + 0.5 * torch.log(Discrim(x_p)).mean() + 0.5 * torch.log(
+        1 - Discrim(y_q)).mean()
+
+    return jsd_objective
+
+def wd_objective(Critic, x_p, y_q):
+
+    wd_objective = Critic(x_p).mean() - Critic(y_q).mean()
+
+    return wd_objective
+
+def gradient_penalty(MLP, x_p, y_q):
+
+    alfa = x_p.size()[0]
+    alfa = torch.rand(alfa, 1)
+    alfa.expand_as(x_p)
+
+    interpolate_z = Variable(alfa * x_p + (1 - alfa) * y_q, requires_grad=True)
+
+    inputs = interpolate_z
+    outputs = Critic(interpolate_z)
+
+    gradients = grad(outputs, inputs, torch.ones(Critic(interpolate_z).size()),
+                     create_graph=True, retain_graph=True, only_inputs=True)[0]
+
+    # gradients = gradients.view(gradients.size(0),  -1)
+    gradient_norm = gradients.norm(2, dim=1)
+    GP = lamda * ((gradient_norm - 1) ** 2).mean()
+    return GP
 
 class MLP(nn.Module):
     def __init__(self, input_dim):
@@ -47,14 +76,16 @@ class jsd_mlp(nn.Module):
     def forward(self, x):
         return self.model(x)
 
+class wd_mlp(nn.Module):
+    def __init__(self, input_dim):
+        super(wd_mlp, self).__init__()
+        self.model = nn.Sequential(
+            MLP(input_dim),
+            F.ReLU()
+        )
 
-def jsd_objective(Discrim, x_p, y_q):
-
-    jsd_objective = torch.log(torch.Tensor([2])) + 0.5 * torch.log(Discrim(x_p)).mean() + 0.5 * torch.log(
-        1 - Discrim(y_q)).mean()
-
-    return jsd_objective
-
+    def forward(self, x):
+        return self.model(x)
 
 def js_divergence(p, q, m_minibatch=1000):
 
@@ -75,45 +106,6 @@ def js_divergence(p, q, m_minibatch=1000):
 
     jsd_exp = jsd_objective(Discrim, x_p, y_q)
     return Discrim, jsd_exp
-
-
-def gradient_penalty(MLP, x_p, y_q):
-
-    alfa = x_p.size()[0]
-    alfa = torch.rand(alfa, 1)
-    alfa.expand_as(x_p)
-
-    interpolate_z = Variable(alfa * x_p + (1 - alfa) * y_q, requires_grad=True)
-
-    inputs = interpolate_z
-    outputs = Critic(interpolate_z)
-
-    gradients = grad(outputs, inputs, torch.ones(Critic(interpolate_z).size()),
-                     create_graph=True, retain_graph=True, only_inputs=True)[0]
-
-    # gradients = gradients.view(gradients.size(0),  -1)
-    gradient_norm = gradients.norm(2, dim=1)
-    GP = lamda * ((gradient_norm - 1) ** 2).mean()
-    return GP
-
-
-class wd_mlp(nn.Module):
-    def __init__(self, input_dim):
-        super(wd_mlp, self).__init__()
-        self.model = nn.Sequential(
-            MLP(input_dim),
-            nn.LeakyReLU()
-        )
-
-    def forward(self, x):
-        return self.model(x)
-
-
-def wd_objective(Critic, x_p, y_q):
-
-    wd_objective = Critic(x_p).mean() - Critic(y_q).mean()
-
-    return wd_objective
 
 def w_distance(p, q, m_minibatch=1000, lamda=10):
     x_p = next(p)
@@ -155,27 +147,3 @@ def q1_3():
         plt.savefig(metric + ".png")
 """
 #q1.3
-lamda = 10
-m_minibatch = 100
-batch_size = 512
-estimated_jsd = []
-estimated_wd = []
-theta = 1 #
-thetas = np.linspace(-1, 1, 21)
-
-for theta in thetas:
-
-    p = iter(distribution1(0))
-    q = iter(distribution1(thetas))
-
-    Discrim, jsd = js_divergence(p, q, m_minibatch)
-    estimated_jsd.append(jsd)
-
-    Critic, wd = w_distance(p, q, m_minibatch, lamda)
-    estimated_wd.append(wd)
-
-plt.plot(phis, estimated_jsd, label="JSD")
-plt.plot(phis, estimated_wd, label="WD")
-plt.legend()
-plt.show()
-plt.savefig('metric_plots.png')
